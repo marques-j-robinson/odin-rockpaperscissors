d09d40b8cfe54badf7b9d1f9b9744f824f7465f2
marques-j-robinson <marques.j.robinson@gmail.com>
2023-10-26 01:16:53 -0700

    create function to return random computer choice

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello, World!')
+//console.log('Hello, World!')
+const choices = ['rock', 'paper', 'scissors']
+const getComputerChoice = () => {
+    const idx = Math.floor(Math.random()*choices.length);
+    return choices[idx]
+}
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
